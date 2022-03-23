bc328469600f7c254e21c8c8cd0c20cfb2659d23
rafael <rafaelviniciuscardoso@gmail.com>
2022-03-23 00:45:29 -0300

    update script

--- install_ingress.sh
+++ install_ingress.sh
@@ -9,10 +9,10 @@ aws eks update-kubeconfig --name $ENVIRONMENT-$REPO_NAME --region $AWS_REGION
 kubectl apply -f templates/rbac-role-alb-ingress-controller.yaml
 
 kubectl annotate serviceaccount -n kube-system alb-ingress-controller \
-eks.amazonaws.com/role-arn=arn:aws:iam::$ACCOUNT_ID:role/role-ingress-controller-$ENVIRONMENT-$REPO_NAME
+eks.amazonaws.com/role-arn=arn:aws:iam::"$ACCOUNT_ID":role/role-ingress-controller-$ENVIRONMENT-$REPO_NAME
 
 cd templates
 
-sed -i 's@# - --cluster-name=devCluster@'"- --cluster-name=$ENVIRONMENT-$REPO_NAME"' alb-ingress-controller.yaml 
+sed -i "s|# - --cluster-name=devCluster|- --cluster-name=$ENVIRONMENT-$REPO_NAME|g" alb-ingress-controller.yaml 
 
 kubectl apply -f alb-ingress-controller.yaml
