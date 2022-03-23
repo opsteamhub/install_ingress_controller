b2716231c95e601866651cc6349f23e8ee01ca07
rafael <rafaelviniciuscardoso@gmail.com>
2022-03-22 22:20:50 -0300

    add files

---
+++ install_ingress.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+kubectl apply -f templates/rbac-role-alb-ingress-controller.yaml
+
+kubectl annotate serviceaccount -n kube-system alb-ingress-controller \
+eks.amazonaws.com/role-arn=arn:aws:iam::$ACCOUNT_ID:role/role-ingress-controller-$ENVIRONMENT-$REPO_NAME
+
+
+sed -i 's/\# - --cluster-name=devCluster/- --cluster-name=$ENVIRONMENT-$REPO_NAME/g' templates/alb-ingress-controller.yaml 
+
+kubectl apply -f templates/alb-ingress-controller.yaml
